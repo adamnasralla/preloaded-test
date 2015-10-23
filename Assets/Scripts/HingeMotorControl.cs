4d030c726a27f87b812d762181fe3248cdf27a1e
Adam Nasralla <adam.nasralla@gmail.com>
2015-10-23 21:48:16 +0200

    Basic player movement

---
+++ Assets/Scripts/HingeMotorControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HingeMotorControl : MonoBehaviour {
+
+    private JointMotor motor;
+    private HingeJoint hinge;
+
+	void Start () 
+    {
+        hinge = GetComponent<HingeJoint>();
+        motor = hinge.motor;
+       // motor.targetVelocity = 0;
+        hinge.motor = motor;
+	}
+
+
+    void SpinAntiClockwise()
+    {
+        motor.targetVelocity = 100f;
+        hinge.motor = motor;
+        hinge.useMotor = true;
+    }
+
+    void SpinClockwise()
+    {
+        motor.targetVelocity = -100f;
+        hinge.motor = motor;
+        hinge.useMotor = true;
+    }
+
+    //Setup Event Listeners
+    void OnEnable()
+    {
+        
+        PlayerInput.MoveRight += SpinAntiClockwise;
+        PlayerInput.MoveLeft += SpinClockwise;
+    }
+
+    void OnDisable()
+    {
+        PlayerInput.MoveRight -= SpinAntiClockwise;
+        PlayerInput.MoveLeft -= SpinClockwise;
+    }
+	
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    public delegate void MoveAction();
+    public static event MoveAction MoveRight;
+    public static event MoveAction MoveLeft;
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (MoveRight != null && Input.GetAxis("Horizontal") > 0) MoveRight();
+        if (MoveLeft != null && Input.GetAxis("Horizontal") < 0) MoveLeft();
+	}
+}
