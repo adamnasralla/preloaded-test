8476f72ab2cc98dd72ec0583012f36c71b99ac05
Adam Nasralla <adam.nasralla@gmail.com>
2015-10-24 14:25:07 +0200

    You can now score points

---
+++ Assets/Scripts/ScoreControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreControl : MonoBehaviour {
+
+    public PickupEventDispatcher eventDispatcher;
+    public ScoreTextControl scoreText;
+    private int score;
+
+
+    void PickupCollected()
+    {
+        score += 50;
+        scoreText.SetScore(score);
+    }
+
+    void OnEnable()
+    {
+        eventDispatcher.PickupCollected += PickupCollected;
+    }
+
+    void OnDisable()
+    {
+        eventDispatcher.PickupCollected -= PickupCollected;
+    }
+}
---
+++ Assets/Scripts/ScoreTextControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreTextControl : MonoBehaviour
+{
+    public Text scoreText;
+    public string zeroString;
+    public string prefix;
+
+    public void SetScore(int score)
+    {
+        scoreText.text = prefix + score.ToString(zeroString);
+    }
+
+}
