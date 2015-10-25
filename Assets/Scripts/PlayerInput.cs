ed936b3dacc9917f4ca0a248e81c21712f5445a0
Adam Nasralla <adam.nasralla@gmail.com>
2015-10-25 02:16:26 +0200

    Three pickup types added

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,9 +5,15 @@ public class PlayerInput : MonoBehaviour {
 
     public MoveEventDispatcher moveEventDispatcher;
 
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
 	void Update () 
     {
-        Time.timeScale += 0.0003f;
+        Time.timeScale += 0.0005f;
+        Debug.Log(Time.timeScale);
         float horizontalAxis = Input.GetAxisRaw("Horizontal");
         if (horizontalAxis > 0) moveEventDispatcher.FireMoveRightEvent();
         if (horizontalAxis < 0) moveEventDispatcher.FireMoveLeftEvent();
--- Assets/Scripts/WallSpawner.cs
+++ Assets/Scripts/WallSpawner.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class WallSpawner : MonoBehaviour {
 
     public GameObject wallPrefab;
+    private int frameCount;
 	// Use this for initialization
 	void Start () 
     {
+        /*
         Debug.Log(transform.position);
         float angle = Vector3.Angle(new Vector3(0, -6, 0), transform.position - new Vector3(0, 6, transform.position.z));
         if (transform.position.x < 0) angle *= -1;
@@ -27,11 +29,29 @@ public class WallSpawner : MonoBehaviour {
             GameObject wall = Instantiate(wallPrefab);
             wall.transform.Rotate(new Vector3(0, 0, angle1));
             wall.transform.position = new Vector3(wall.transform.position.x, wall.transform.position.y + 6f, transform.position.z);
+        }*/
+	}
+	
+
+	void Update () 
+    {
+        frameCount++;
+        if (frameCount == 100)
+        {
+            frameCount = 0;
+            SpawnWall();
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void SpawnWall()
+    {
+        for (int i = -4; i < 5; i++)
+        {
+            float angle = 30f*i;
+            GameObject wall = Instantiate(wallPrefab);
+            wall.transform.Rotate(new Vector3(0, 0, angle));
+            wall.transform.position = new Vector3(wall.transform.position.x, wall.transform.position.y + 6f, transform.position.z);
+        }
+    }
+
 }
